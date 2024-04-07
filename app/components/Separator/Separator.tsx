c376603e855d0ab765d6fa8eb85be63f660cb8b9
jayanth920 <113549918+jayanth920@users.noreply.github.com>
2024-04-06 20:26:51 -0500

    lil mods

--- app/components/Separator/Separator.tsx
+++ app/components/Separator/Separator.tsx
@@ -2,7 +2,7 @@
 
 const Separator = () => {
     return (
-        <div className="h-1 w-[50vw] bg-white my-36">
+        <div className="h-1 w-[50vw] bg-white my-36 blur-[2px]">
         </div>
     )
 }
--- app/components/Tech/Tech.tsx
+++ app/components/Tech/Tech.tsx
@@ -1,13 +1,12 @@
 
 import React from 'react'
-import { CanvasRevealEffectDemo } from '../CanvasPhoto/CanvasPhoto'
-import { SpotlightPreview } from '../SpotLight/SpotLight'
+
 
 
 const Tech = () => {
     return (
         <div className="flex justify-center text-white ">
-
+            Project
         </div>
     )
 }
